e4715a938f9c2e28dbfaa33f63861f8548968782
Paula Sousa <paulasousa137@gmail.com>
2023-05-15 12:46:58 +0200

    enlace con API

--- rick-morty-app/src/app/personajes/personajes.component.ts
+++ rick-morty-app/src/app/personajes/personajes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { PersonajesService } from '../personajes.service';
 
 @Component({
   selector: 'app-personajes',
@@ -7,24 +7,22 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./personajes.component.css']
 })
 export class PersonajesComponent implements OnInit {
-   //Declara una propiedad characters como un arreglo vacío ([]) de tipo any[]. 
-   //Esta propiedad se utilizará para almacenar los personajes obtenidos del archivo JSON.
   characters: any[] = [];
 
-  // Inyección de HttpClient en el constructor
-  constructor(private http: HttpClient) {}
+  constructor(private personajesService: PersonajesService) {}
 
   ngOnInit() {
-    // Llama al método para obtener los personajes al inicializar el componente
-    this.fetchCharacters();
-  }
-
-  fetchCharacters() {
-     // Realiza una solicitud HTTP GET al archivo JSON
-    this.http.get<any>('assets/database.json').subscribe(data => {
-      this.characters = data.characters;
-    });
+    this.personajesService.fetchCharacters().subscribe(
+      result => {
+        if (result.results && Array.isArray(result.results)) {
+          this.characters = result.results;
+        } else {
+          console.error('API response is not as expected:', result);
+        }
+      },
+      error => {
+        console.error('Error fetching characters:', error);
+      }
+    );
   }
 }
-
-
